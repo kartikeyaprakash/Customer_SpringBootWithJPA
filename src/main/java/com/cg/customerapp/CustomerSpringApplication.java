313bf1f7ec8727c0298212d83713fc2f2586e2aa
kartikeyaprakash <kartikeya1899@gmail.com>
2020-10-30 12:35:00 +0530

    EnableTransactionManagement and table name corrected

--- src/main/java/com/cg/customerapp/CustomerSpringApplication.java
+++ src/main/java/com/cg/customerapp/CustomerSpringApplication.java
@@ -3,9 +3,11 @@ package com.cg.customerapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.cg.customerapp.ui.ProjectUi;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class CustomerSpringApplication {
 
--- src/main/java/com/cg/customerapp/entities/Customer.java
+++ src/main/java/com/cg/customerapp/entities/Customer.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Table(name = "students")
+@Table(name = "customers")
 @Entity
 public class Customer {
 	
